9085353d045d2adc022dc7dec1f1da5bbdd5af08
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-01-28 10:54:57 +0530

    fix : player name set and display

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import { flushSync } from "react-dom";
+
 export default function Player() {
+  const [playerName,setPLayerName] = useState(null);
+  const [isSubmmited,setIsSubmmited] = useState(false);
+
+  function handelChange(event){
+    setIsSubmmited(false)
+    setPLayerName(event.target.value)
+  }
+
+  function onSubmit(){
+    setIsSubmmited(true)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isSubmmited ? playerName :'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text"  value={playerName} onChange={handelChange}/>
+        <button onClick={onSubmit}>Set Name</button>
       </p>
     </section>
   );
